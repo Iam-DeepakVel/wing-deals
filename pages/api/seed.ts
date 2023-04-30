ac086a26f9ece56d4c1e0342dea204dbb66a7096
iam-deepakvel <deepakvel82@gmail.com>
2023-04-30 12:37:49 +0530

    feat: db connection & seeding db

---
+++ pages/api/seed.ts
@@ -0,0 +1,14 @@
+import User from "@/models/User";
+import data from "@/utils/data";
+import db from "@/utils/db/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await db.disconnect();
+  res.send({ message: "Seeded Successfully" });
+};
+
+export default handler;
